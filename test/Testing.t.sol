86dfb3f70f16494eef39289ed2c69476a4b7001b
Icarus <58011919+Benj1j@users.noreply.github.com>
2024-03-05 22:12:00 +0100

    Update and rename Test.t.sol to Testing.t.sol

--- test/Testing.t.sol
+++ test/Testing.t.sol
@@ -1,18 +1,16 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^X.X.XX;//Add the version you need.
+pragma solidity ^X.X.XX;//Change to the version you need to be compatible.
 
 import "forge-std/Test.sol";
 import "forge-std/console.sol";
-import "../src/chall.sol";
+import "../src/XXX.sol"; //Import the contract you want here.
 
-contract ContractTest is Test {
+contract Testing is Test {
 
     function setUp() public {
     }
 
     function testExploit() public {
-        vm.createSelectFork(vm.rpcUrl("CTFnetwork"));
-        
-
     }
 }
+
